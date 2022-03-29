8db4149acd0259b6388a4c8191018bfd23f2a4a7
Marcelo Braga <marcelobraga.dev@gmail.com>
2022-03-29 08:47:02 -0300

    removendo bug de classe

--- includes/shortcodes/class.php
+++ includes/shortcodes/class.php
@@ -10,7 +10,6 @@ new \TudoClassificados\App\src\Shortcodes\Anuncios\Classificados\Templates\Grid(
 
 
 require_once TUDOCLASSIFICADOS_PATH . 'includes/classes/Anuncios/Templates/Cards.php';
-require_once TUDOCLASSIFICADOS_PATH . 'includes/classes/Anuncios/Templates/Marketplace/ListaAnunciosMarketplace.php';
 
 new TudoClassificados\App\Views\Pages\Imoveis\Anuncios\Index;
 new Cards();
